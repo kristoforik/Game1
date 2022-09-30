4f925f042f1a6e3889238de15f3c03d870fbcba8
Andrii Kudenko <kudenko4614@gmail.com>
2022-09-30 01:57:11 -0400

    added challenge 3 for the first role

--- Game.py
+++ Game.py
@@ -1,6 +1,6 @@
 class Challenge1_1:
     def introduction(self):
-        print("The operation has started...\nThe first task is to hack electrical system and cut the power of the building\nRoll the dice to hack the system. Press [1] to roll")
+        print("The operation has started...\nThe first task is to hack electrical system and cut the power of the building\nRoll the dice to hack the system. Type [1] to roll")
     def success(self):
         print("Well done! You hacked electricity and now terrorists don't have light.")
     def fail(self):
@@ -9,7 +9,7 @@ class Challenge1_1:
         print("Well done! You hacked electricity and improved your hacking abilities by 1 so that now terrorists don't have light.")
 class Challenge2_1:
     def introduction(self):
-        print("Get ready your rifle! Your next task is to enter a building and kill all terrorits there...\nRoll the dice to begin an anti-terror sweep. Press [1] to roll")
+        print("Get ready your rifle! Your next task is to enter a building and kill all terrorits there...\nRoll the dice to begin an anti-terror sweep. Type [1] to roll")
     def success(self):
         print("Good job! All terrorists are dead.")
     def fail(self):
@@ -18,4 +18,15 @@ class Challenge2_1:
         print("Great job! You killed all terrorists and improved your shooting abilities by 1.")
     def full_fail(self):
         print("Unfortunatelly, you were killed and mission is lost.")
-        
+class Challenge3_1:
+    def inroduction(self):
+        print("You are almost there! Your final task is to crack the door and defuse the bomb attached to the hostage\nRoll the dice to begin cracking and defusing. Type [1] to roll")
+    def succes(self):
+        print("Awesome, YOU WON! Terrorists are dead and hostage is saved. Mission is done successfully.")
+    def fail(self):
+        print("YOU LOST! You didn't manage to defuse the bomb and exploded together with a hostage(")
+    def full_success(self):
+        print("YOU WON! Terrorists are dead and hostage is saved. Mission is done successfully and you improved your defusing abilities by 1.")
+    def full_fail(self):
+        print("YOU LOST! You didn't even manage to crack the door, so the building blew up")
+    
