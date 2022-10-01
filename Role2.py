f0a11891c33429dbf266f95917431414bde8f46e
Andrii Kudenko <kudenko4614@gmail.com>
2022-09-30 23:29:28 -0400

    added second role

---
+++ Role2.py
@@ -0,0 +1,13 @@
+name = 'Roger'
+age = 43
+shooting = 1
+hacking = 2
+cracking = 1
+class Description:
+    def description(self):
+        print("Operator's description:")
+        print("   Name:", name)
+        print("   Age:", age)
+        print("   Shooting accuracy:", shooting)
+        print("   Hacking:", hacking)
+        print("   Cracking:", cracking)
