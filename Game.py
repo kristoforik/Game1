a92435190f262fe20705f9240b240265f262d1ab
Andrii Kudenko <kudenko4614@gmail.com>
2022-10-02 22:55:35 -0400

    debugging...

--- Game.py
+++ Game.py
@@ -14,20 +14,20 @@ class Challenge1_1:
     def introduction(self):
         print_slowly("The operation has started...\nThe first task is to hack electrical system and cut the power of the building\nRoll the dice to hack the system. Type [1] to roll ")
     def success(self):
-        print_slowly("Well done! You hacked electricity and now terrorists don't have light.")
+        print_slowly("Well done! You hacked electricity and now terrorists don't have light. ")
     def fail(self):
-        print_slowly("You lost! You did not manage to hack electricity and time is out.") 
+        print_slowly("You lost! You did not manage to hack electricity and time is out. ") 
     def full_success(self):
-        print_slowly("Well done! You hacked electricity and improved your hacking abilities by 1 so that now terrorists don't have light.")
+        print_slowly("Well done! You hacked electricity and improved your hacking abilities by 1 so that now terrorists don't have light. ")
 class Challenge2_1:
     def introduction(self):
         print_slowly("Get ready your rifle! Your next task is to enter a building and kill all terrorits there...\nRoll the dice to begin an anti-terror sweep. Type [1] to roll ")
     def success(self):
-        print_slowly("Good job! All terrorists are dead.")
+        print_slowly("Good job! All terrorists are dead. ")
     def fail(self):
-        print_slowly("Unfortunatelly, while entering the building you got injured and your ability was reduced by 1. However, due to your experience, you have the second chance to continue the mission. Type [1] to roll again")
+        print_slowly("Unfortunatelly, while entering the building you got injured and your ability was reduced by 1. However, due to your experience, you have the second chance to continue the mission. Type [1] to roll again ")
     def full_success(self):
-        print_slowly("Great job! You killed all terrorists and improved your shooting abilities by 1.")
+        print_slowly("Great job! You killed all terrorists and improved your shooting abilities by 1. ")
     def full_fail(self):
         print_slowly("Unfortunatelly, you were killed and mission is lost.")
 class Challenge3_1:
@@ -45,22 +45,22 @@ class Challenge1_2:
     def introduction(self):
         print_slowly("The operation has started...\nThe first task is to hack electrical system and cut the power of the building\nRoll the dice to hack the system. Type [1] to roll ")
     def success(self):
-        print_slowly("Well done! You hacked electricity and now terrorists don't have light.")
+        print_slowly("Well done! You hacked electricity and now terrorists don't have light. ")
     def fail(self):
-        print_slowly("Unfortunatelly, you didn't manage to hack the system this time and your hacking abilities were reduced by 1. However, you got a new device which might help you to shut power down, you have the second chance to continue the mission. Type [1] to roll")
+        print_slowly("Unfortunatelly, you didn't manage to hack the system this time and your hacking abilities were reduced by 1. However, you got a new device which might help you to shut power down, you have the second chance to continue the mission. Type [1] to roll ")
     def full_fail(self):
-        print_slowly("You lost! You did not manage to hack electricity and time is out.") 
+        print_slowly("You lost! You did not manage to hack electricity and time is out. ") 
     def full_success(self):
-        print_slowly("Well done! You hacked electricity and improved your hacking abilities by 1 so that now terrorists don't have light.")
+        print_slowly("Well done! You hacked electricity and improved your hacking abilities by 1 so that now terrorists don't have light. ")
 class Challenge2_2:
     def introduction(self):
         print_slowly("Get ready your rifle! Your next task is to enter a building and kill all terrorits there...\nRoll the dice to begin an anti-terror sweep. Type [1] to roll ")
     def success(self):
         print_slowly("Good job! All terrorists are dead.")
     def full_success(self):
-        print_slowly("Great job! You killed all terrorists and improved your shooting abilities by 1.")
+        print_slowly("Great job! You killed all terrorists and improved your shooting abilities by 1. ")
     def fail(self):
-        print_slowly("Unfortunatelly, you were killed and mission is lost.")
+        print_slowly("Unfortunatelly, you were killed and mission is lost. ")
 class Challenge3_2:
     def inroduction(self):
         print_slowly("You are almost there! Your final task is to crack the door and defuse the bomb attached to the hostage\nRoll the dice to begin cracking and defusing. Type [1] to roll ")
